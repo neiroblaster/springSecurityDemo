a813615e7feaaef5c8d53f99037d9d5a540c4680
neiroblaster <neiroblaster@gmail.com>
2021-08-03 17:27:49 +0300

    part

---
+++ src/main/java/net/proselyte/springsecuritydemo/rest/DeveloperRestControllerV1.java
@@ -0,0 +1,42 @@
+package net.proselyte.springsecuritydemo.rest;
+
+import net.proselyte.springsecuritydemo.model.Developer;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@RestController
+@RequestMapping("/api/v1/developers")
+public class DeveloperRestControllerV1 {
+    private List <Developer> DEVELOPERS = Stream.of(
+            new Developer(1L, "Den", "Sh"),
+            new Developer(2L, "Mike", "Br"),
+            new Developer(3L, "Jhon", "Dm")
+    ).collect(Collectors.toList());
+
+
+    @GetMapping
+    public List<Developer> getAll(){
+        return DEVELOPERS;
+    }
+
+    @GetMapping("/{id}")
+    public Developer getById (@PathVariable Long id){
+        return DEVELOPERS.stream().filter(developer -> developer.getId().equals(id))
+                .findFirst()
+                    .orElse(null);
+    }
+
+    @PostMapping
+    public Developer create (@RequestBody Developer developer){
+        this.DEVELOPERS.add(developer);
+        return developer;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable Long id){
+        this.DEVELOPERS.removeIf(developer -> developer.getId().equals(id));
+    }
+}
